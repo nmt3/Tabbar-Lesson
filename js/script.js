914d3418112558191fa2613327b9a7a159ed793b
nmt3 <nmttkm3@gmail.com>
2023-05-06 00:49:13 +0000

    fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$("#tab-contents .tab[id!="tab1"]").hide();
+$('#tab-contents .tab[id != "tab1"]').hide();
 
-$('tab1-menu a').on('click',function(event){
+$('#tab-menu a').on('click', function(event) {
   $("#tab-contents .tab").hide();
-  $("#tab-menu .activ").removeClass("activ");
-  $(this).addClass('activ');
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass('active');
   $($(this).attr('href')).show();
   event.preventDefault();
 });
